f8d71db2abf491ffe0976c6c4f2b0e0e51e9f9a5
Eva Clark <101047194+evaclark@users.noreply.github.com>
2022-09-27 21:33:53 +0930

    Add files via upload

--- discovery_piscine/cell00/ex04/argv.sh
+++ discovery_piscine/cell00/ex04/argv.sh
@@ -3,10 +3,12 @@
 if [ -z "$1" ]; then
 	echo "No arguments supplied"
 	exit 1
-elif [ $# -eq 3 ]; then
+elif [ $# -eq 1 ]; then
+	echo "$1"
+	exit 1
+elif [ $# -eq 2 ]; then
 	echo "$1"
 	echo "$2"
-	echo "$3"
 	exit 1
 else
 	echo "$1"
